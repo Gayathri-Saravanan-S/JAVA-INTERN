f5cf18138712a35fdc08498bff58ea8fe5230705
Gayathri Saravanan <gayathrisasikala99@gmail.com>
2025-08-14 17:28:53 +0530

    Add files via upload

---
+++ primitive data type.java.java
@@ -0,0 +1,21 @@
+class Main{
+    public static void main(String[] args)
+    {
+    int a = 10;
+    float b = 37.56f;
+    long c = 19758;
+    byte d = 20;
+    short e = 5000;
+    double f = 10.9d;
+    char g = 's';
+    String h = "vivo";
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+    System.out.println(f);
+    System.out.println(g);
+    System.out.println(h);
+    }
+}
